2c1ad902b5d7f90774e49fb7634ca56c92070d65
Marioccarneiro <mario.ccarneiro@hotmail.com>
2023-07-12 15:31:53 -0300

    integracao-apis-I

--- src/componentes/Musicas/styled.js
+++ src/componentes/Musicas/styled.js
@@ -1,34 +1,49 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const ContainerMusicas = styled.article`
-    @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap');
-    font-family: 'Raleway', sans-serif;    
-    border: 6px double black;
-    width: 65vw;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 32px 0;
-    margin: 16px;
-    min-width: fit-content;
-`
+  @import url("https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap");
+  font-family: "Raleway", sans-serif;
+  border: 6px double black;
+  width: 65vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 32px 0;
+  margin: 16px;
+  min-width: fit-content;
+  gap: 10px;
+`;
 export const Musica = styled.section`
-    display: flex;
-    align-items: center;
-    gap: 8px;
-    box-sizing: border-box;
-    flex-wrap:wrap;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+`;
 export const ContainerInputs = styled.div`
-    width: 100% ;
-    display: flex;
-    margin-top: 16px;
-    justify-content: center;
-`
+  width: 100%;
+  display: flex;
+  margin-top: 16px;
+  justify-content: center;
+`;
 export const InputMusica = styled.input`
-    width: 15vw;
-`
+  width: 15vw;
+`;
 
 export const Botao = styled.button`
-    width: 10vw;
-`
+  width: 10vw;
+`;
+
+export const Titles = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h3 {
+    margin: 0;
+  }
+  h4 {
+    margin: 0;
+  }
+`;
